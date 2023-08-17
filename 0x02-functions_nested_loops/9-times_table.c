869b77859790745976533d6c05151df5e5041f3f
OmarElmelegy <elmelegy.omar@yahoo.com>
2023-08-16 18:54:25 -0700

    fuck spaces

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
+/**
+ * times_table - main function
+ *
+ * Return: Nothing
+ *
+ */
+
 void times_table(void)
 {
-	int i;
-	int j;
+	int i, j;
+
 		for (i = 0; i <= 9; i++)
 		{
 			for (j = 0; j <= 9; j++)
 			{
 				int prod = i * j;
+
 				if (j == 0)
 				{
 					_putchar('0');
@@ -16,7 +24,8 @@ void times_table(void)
 				else if (prod <= 9)
 				{
 					_putchar(',');
-					_putchar(' ');					                                _putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(prod + '0');
 				}
 				else
