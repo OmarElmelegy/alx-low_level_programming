46912790a6d4531793c5954c09bb6aeb8d0c0e3e
OmarElmelegy <elmelegy.omar@yahoo.com>
2023-08-25 18:57:28 -0700

    add 0x05 basic

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
+*/
+
+
+void reset_to_98(int *n)
+{
+	if (*n != 98)
+	{
+		*n = 98;
+	}
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
+
+
+void _puts(char *str)
+{
+	int i = 0, j;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
